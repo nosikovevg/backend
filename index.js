45f2f87fb1727cab95cc1799ff54117047ad26e4
Nos <zvezdazanatih@gmail.com>
2025-04-29 11:14:15 +0700

    1

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const mysql2 = require ('mysql2/promise');
+const pool = mysql2.createPool ({
+    host: 'localhost',
+    user: 'root',
+    database: 'expo',
+    password: '1234',
+});
+
+
+const app = express();
+
+
+app.get('/', function (req, res) {
+    pool.query('SELECT * FROM tab1').then(function(data) {
+        res.json(data);
+    });
+   
+//    res.send("It's working!!!!!!!!!!");
+
+
+})
+
+
+app.listen(3000, ()=> {
+    console.log("It's started", new Date());
+});
